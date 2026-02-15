979418418b283f5181d95e9ce2d47ea3e70c7054
Gilson <gilsonrusso@outlook.com>
2026-02-15 18:22:31 -0400

    Install Python project dependencies.

---
+++ app/api/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+api_router = APIRouter()
+
+# TODO: Import and include routers from endpoints
+# form app.api.v1.endpoints import projects, agents ...
+# api_router.include_router(projects.router, prefix="/projects", tags=["projects"])
